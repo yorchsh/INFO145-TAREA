2d37c4bfe7f76695611dfe2b1616d8508fc4dfa6
Sade <javier.ramirez02@alumnos.uach.cl>
2026-06-04 19:13:25 -0400

    Refactor vector_generation.hpp by removing uniformVector
    Removed the uniformVector function and unnecessary comments.

--- include/vector_generation.hpp
+++ include/vector_generation.hpp
@@ -8,24 +8,6 @@ namespace vec_gen {
     static random_device rd;
     static mt19937 generator(rd());
 
-    // este probablemente matará uno que otro PC
-    template<typename T>
-    vector<T> uniformVector(int size) {
-        vector<T> v;
-        v.reserve(size);
-
-        uniform_int_distribution<T> distribution(
-            numeric_limits<T>::min(),
-            numeric_limits<T>::max()
-        );
-
-        for (int i = 0; i < size; i++)
-            v.push_back(distribution(generator));
-
-        return v;
-    }
-
-    // este lo usamos en Todo
     template<typename T>
     vector<T> normalVector(int size, float64_t standard_deviation) {
         vector<T> v;
@@ -42,7 +24,6 @@ namespace vec_gen {
         return v;
     }
     
-    // esto debemos usarlo en todos los Casos
     template<typename T>
     std::vector<T> linealVector(int size, T epsilon) {
         std::vector<T> v;
