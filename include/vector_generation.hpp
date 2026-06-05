561de9510ef7c13705e405ea622d04294ecc5f29
Sade <javier.ramirez02@alumnos.uach.cl>
2026-06-05 09:25:09 -0400

    Update vector_generation.hpp
    rather not use using namespace std

--- include/vector_generation.hpp
+++ include/vector_generation.hpp
@@ -2,26 +2,22 @@
 
 #include <bits/stdc++.h>
 
-
-
 namespace vec_gen {
-    using namespace std;
-
-    static random_device rd;
-    static mt19937 generator(rd());
+    static std::random_device rd;
+    static std::mt19937 generator(rd());
 
     template<typename T>
-    vector<T> normalVector(int size, float64_t standard_deviation) {
-        vector<T> v;
+    std::vector<T> normalVector(int size, std::float64_t standard_deviation) {
+        std::vector<T> v;
         v.reserve(size);
-        // normal_distribution d{5.0, 2.0};
-        normal_distribution<float64_t> distribution(
+        // std::normal_distribution d{5.0, 2.0};
+        std::normal_distribution<std::float64_t> distribution(
             0.0,
             standard_deviation
         );
 
         for (int i = 0; i < size; i++)
-            v.push_back(round(distribution(generator)));
+            v.push_back(std::round(distribution(generator)));
 
         return v;
     }
